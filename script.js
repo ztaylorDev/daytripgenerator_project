4653b9a8f158db5bd5525a76505a4b310b56e992
Zachary Taylor <ztaylor@Zacharys-MacBook-Air.local>
2021-06-21 21:00:03 -0400

    my first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+function getRandomColor(color){
+    return color;
+    
+}
+
+
+
+
+color = ["purple", "blue", "orange", "black"];
+let getRandomNumber = math.random() * color;
+getRandomColor = math.floor(getRandomNumber) + 1;
+console.log(getRandomNumber);
+
