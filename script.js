d3d74fa2150093fe6e8ee05a7cdd4f576be36977
Zachary Taylor <ztaylor@Zacharys-MacBook-Air.local>
2021-06-22 18:12:27 -0400

    created randomley generated functions

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
 
-
-function getRandomColor(color){
-    return color;
+function getRandomFood(restaurants){
+    return sourceOfFood;
     
 }
 
+    let sourceOfFood = ["moes", "dominoes", "rocos tacos"];
+    let food = sourceOfFood[Math.floor(Math.random() * sourceOfFood.length)];
+    console.log(food);
 
 
 
-color = ["purple", "blue", "orange", "black"];
-let getRandomNumber = Math.random() * color;
-getRandomColor = Math.floor(getRandomNumber) + 1;
-console.log(getRandomNumber);
+
+
+
+
+
+
+
+// let generatedTransportation = getRandomSelection(transportation);
+
+function getRandomTransportation(vehicle){
+    return transportation;
+}
+    let transportation = ["bus", "train", "plane"];
+    let modeOfTranspo = transportation[Math.floor(Math.random() * transportation.length)];
+    console.log(modeOfTranspo); 
+
+
+
+
+
+// let generateddestination = getRandomSelection(destination);
+
+function getRandomDestination(destiny){
+    return destination;
+}
+    let destination = ["hawaii", "tampa", "texas"];
+    let randomLocation = destination[Math.floor(Math.random() * destination.length)];
+    console.log(randomLocation); 
+
+
+
+
+
+// let generatedentertainment = getRandomSelection(entertainment);
+function getRandomEntertainment(showtime){
+    return entertainment;
+}
+    let entertainment = ["comedy show", "concert", "movie"];
+    let randomEntertainment = entertainment[Math.floor(Math.random() * entertainment.length)];
+    console.log(randomEntertainment); 
+
+
+
+
+
+
+
+// function getRandomColor(color){
+//     return color;
+// }
+
+// color = ["purple", "blue", "orange", "black"]
+// let getRandomNumber = Math.random() * 4;
+// getRandomNumber = Math.floor(getRandomNumber) + 1;
+// console.log(getRandomNumber);
+
+
+
+
+
+
+
 
